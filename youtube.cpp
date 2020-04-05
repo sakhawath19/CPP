4932421a9f4084388665e150320dfcf2c187b096
sakhawath <sakhawath19@gmail.com>
2020-04-05 13:28:30 +0300

    all in one tutorial series

---
+++ youtube.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main()
+{
+    int slices;
+    cout << "Please insert a number: ";
+    cin >> slices;
+
+    cout <<"You have entered " << slices <<std::endl;
+
+}
+
+
+/*
+#include <iostream>
+
+using std::cout;
+
+// endl is a manipulator
+int main()
+{
+    int slice = 5;
+    cout << "You have " << slice <<" pieces of pizza"<<std::endl; 
+
+    //printf("%i\n", slice);
+}
+*/
+
+/*
+#include <iostream>
+
+using std::cout;
+
+int main()
+{
+    int slice; //decleration 
+    slice = 5; //initialization
+
+    int children = 4 + 3;
+
+    cout << children;
+}
+*/
+
+
+
+/*
+//preprocessor include iostream into our code
+#include <iostream>
+
+//using namespace std; // using directive
+
+//using std::cout; // only declaring cout from standard container
+
+//std-namespace
+//for example Bill and Sujan both created cout 
+//then lets use Bill's cout 
+//Bill:cout - something like that from Bill's container
+//in our code cout is coming from standard namespace named std 
+//it is better to use prefix to avoid namespace conflict
+//'<<' is a operator. both side has its operand
+//it takes the input string from one side and give it to the other side object to print it in console
+
+
+int main(){
+    std::cout << "Hello World\n";
+    return 0;
+}
+
+*/
