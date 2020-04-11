6e14db6a984dc23f9a7c6da40f1335acc1aea649
sakhawath <sakhawath19@gmail.com>
2020-04-11 10:32:12 +0300

    method of constructor initiated

---
+++ method_constructor.cpp
@@ -0,0 +1,10 @@
+//funtion
+//method
+//static method
+//constructor
+//destructor
+
+//static method attached to a class
+//method attached to a object
+
+//constructor does not have return type type
