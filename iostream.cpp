86fea666d6d1be7d42aab0593709aedd67f7e75f
sakhawath <sakhawath19@gmail.com>
2020-04-22 14:31:28 +0300

    iostream.cpp

--- iostream.cpp
+++ iostream.cpp
@@ -1,3 +1,8 @@
+// std::cout is used to output a value (cout = character output)
+// std::cin is used to get an input value (cin = character input)
+// << is used with std::cout, and shows the direction that data is moving
+// \n is preffered over std::endl to avoid flashing 
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -5,9 +10,44 @@
 
 int main()
 {
+    std::cout << "Enter a number: "; // ask user for a number
+    int x{ }; // define variable x to hold user input (and zero-initialize it)
+    std::cin >> x; // get number from keyboard and store it in variable x
+    std::cout << "You entered " << x << '\n';
     return 0;
 }
 
+
+/*
+int main()
+{
+    std::ifstream file ("hello.txt");
+    
+    std:: string line;
+    getline(file, line);
+
+    std::cout << line << std::endl;
+    
+    return 0;
+}*/
+
+/*
+int main()
+{
+    std::ifstream file ("hello.txt");
+    std::vector<std::string> names;
+    std::string input;
+    while(file >> input)
+    {
+        names.push_back(input);
+    }
+
+    for(std::string name: names)
+    {
+        std::cout << name << std::endl;
+    }
+    return 0;
+} */
 /*
 int main()
 {
