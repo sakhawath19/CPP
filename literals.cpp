4fe5392959e6ef377f60bb564539223319e7ff23
sakhawath <sakhawath19@gmail.com>
2020-04-22 14:29:20 +0300

    literals.cpp

---
+++ literals.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    auto a = 5U;
+    auto b = 4UL;
+    auto c = 1ULL;
+
+    auto x = 5.5; //without literal constant it is double
+    auto y = 5.F;
+
+    int number = 30;
+ }
