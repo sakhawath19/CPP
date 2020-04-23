c0167a9a04e405bb2d7e751f44926d1d305d5843
sakhawath <sakhawath19@gmail.com>
2020-04-23 14:17:48 +0300

    function.cpp modified

--- function.cpp
+++ function.cpp
@@ -1,3 +1,66 @@
+/*
+Function can //call another //function 
+One function can not be defined inside another function 
+*/
+
+
+
+/* Function forward decleration
+Function have to be defined before main function otherwise
+function can be declared before main function but can be defined later
+    
+#include <iostream>
+
+// int add(int, int) // this is also valid decleration// but best practice to use 
+// function parameters
+int add(int x, int y); // forward declaration of add() (using a function prototype)
+ 
+int main()
+{
+    std::cout << "The sum of 3 and 4 is: " << add(3, 4) << '\n'; // this works because we forward declared add() above
+    return 0;
+}
+ 
+int add(int x, int y) // even though the body of add() isn't defined until here
+{
+    return x + y;
+}
+
+*/
+
+
+
+/* Fixing the order of parameters
+If the architecture evaluates left to right, 
+a() will be called before b(). If the architecture evaluates right to left, 
+b() will be called before a(). This may or may not be of consequence, depending on what a() and b() do.
+
+If it is important that one argument evaluate first, 
+you should explicitly define the order of execution, like so:
+
+
+someFunction(a(), b());
+
+int avar{ a() }; // a() will always be called first
+int bvar{ b() }; // b() will always be called second
+ 
+someFunction(avar, bvar); // it doesn't matter whether avar or bvar are copied first 
+because they are just values
+*/
+
+
+
+/* order of the variable cration and destruction
+int add(int x, int y)
+{ 
+    int z{ x + y };
+ 
+    return z;
+} // z, y, and x sequentially destroyed here // local variables are destroyed in opposite order of creation
+*/
+
+
+
 #include <iostream>
 
 using std::cout;
@@ -11,7 +74,9 @@ void print_pow(double base, int exponent)
 int main()
 {
     print_pow(2, 3);
+    return EXIT_SUCCESS; // EXIT_FAILURE
 }
+
 
 
 /*
@@ -44,6 +109,9 @@ double power_cal(double base, int exponent)
     }
     return result;
 }*/
+
+
+
 /*
 #include <iostream>
 
