1ac9f90d519a3e0cae9fa47c7acf7a139efaffb8
sakhawath <sakhawath19@gmail.com>
2020-04-23 12:16:08 +0300

    constant.cpp

--- constant.cpp
+++ constant.cpp
@@ -1,3 +1,34 @@
+
+//example of constant modifier 
+#include <iostream>
+#include <vector>
+
+void do_something(const int data[])
+{
+
+}
+
+void print_array(const int data[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        //this will modify the original data without data modifier is defiend as const
+        //data[i]++; 
+        std::cout << data[i] << "\t";
+    }
+    std::cout << "\n";
+    do_something(data);
+}
+
+int main()
+{
+    int data[] = {1,2, 3};
+    print_array(data, 3);
+
+    std::cout << data[0] << std::endl;
+    return 0;
+}
+/*
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -15,4 +46,4 @@ int main()
     cout << X << endl;
     cout << y << endl;
     return 0;
-}
+}*/
