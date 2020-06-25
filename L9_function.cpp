591d5c46b7607748e48730b9ce9d61380dab0865
sakhawath <sakhawath19@gmail.com>
2020-06-25 17:27:11 +0300

    L9_function.cpp updated

---
+++ L9_function.cpp
@@ -0,0 +1,60 @@
+/*
+Passing arguments by reference
+*/
+
+/*
+#include <iostream>
+#include <cmath>    // for std::sin() and std::cos()
+ 
+void getSinCos(double degrees, double &sinOut, double &cosOut)
+{
+    // sin() and cos() take radians, not degrees, so we need to convert
+    static constexpr double pi { 3.14159265358979323846 }; // the value of pi
+    double radians = degrees * pi / 180.0;
+    sinOut = std::sin(radians);
+    cosOut = std::cos(radians);
+}
+ 
+int main()
+{
+    double sin(0.0);
+    double cos(0.0);
+ 
+    // getSinCos will return the sin and cos in variables sin and cos
+    getSinCos(30.0, sin, cos);
+ 
+    std::cout << "The sin is " << sin << '\n';
+    std::cout << "The cos is " << cos << '\n';
+    return 0;
+}
+*/
+
+#include <iostream>
+
+using std::cout; 
+
+class Rectange
+{
+    int width;
+    int height;
+
+    public: 
+        Rectangle(int w, int h)
+        {
+            this -> width = w;
+            this -> height = h; 
+        }
+
+        int get_area()
+        {
+            return width * height; 
+        }
+}
+
+int main()
+{
+    constexpr Rectange rec(5, 6);
+    cout << "Area" << rec.get_area() << std::endl;
+
+    return 0;
+}
