06ddaef4b7db27273dd21ac570571fe190b4d640
sakhawath <sakhawath19@gmail.com>
2020-06-25 17:25:52 +0300

    L7_pointer.cpp updated

--- L7_pointer.cpp
+++ L7_pointer.cpp
@@ -24,6 +24,7 @@ int main()
     std::cout << "*(&value): " << *(&value) << std::endl;                 // prints 5
     
     // **pointer pointing to the address of integer value
+    void *vptr = NULL;                                   // pointer has the address of null 
     int *ptr;                                            // a pointer to an integer value
     ptr = &value;                                         // ptr points to the address of value 
     std::cout << "ptr: " << ptr << std::endl;                       // prints address held in ptr, which is &value
@@ -41,7 +42,10 @@ int main()
     int array[]{1, 3, 5, 7};
     int *aptr{array};
     std::cout << "*aptr: " << *aptr << std::endl;
-    std::cout << "Index refering value of address: " << aptr[0] << std::endl;
+    std::cout << "Address of the first elements " << &aptr[0] << std::endl;
+    std::cout << "Address of the second elements " << (aptr + 1) << std::endl; // Idea of pointer arithmatic 
+    std::cout << "Value of the first index " << aptr[0] << std::endl;
+    std::cout << "Value of the third index " << *(aptr+2) << std::endl;         
 
     // Use nullptr to initialize your pointers to a null value.
     // dereferencing null pointer would crash the program or would give garbage value
@@ -53,3 +57,51 @@ int main()
 
     return 0;
 }
+
+
+// Example of void pointer
+// Avoid using void pointer unless it is absolutely necessary
+// Void pointer can point to any data type but direct dereferencing is not possible
+// need to cast it 
+
+/*
+#include <iostream>
+ 
+enum class Type
+{
+    INT,
+    FLOAT,
+    CSTRING
+};
+ 
+void printValue(void *ptr, Type type)
+{
+    switch (type)
+    {
+        case Type::INT:
+            std::cout << *static_cast<int*>(ptr) << '\n'; // cast to int pointer and dereference
+            break;
+        case Type::FLOAT:
+            std::cout << *static_cast<float*>(ptr) << '\n'; // cast to float pointer and dereference
+            break;
+        case Type::CSTRING:
+            std::cout << static_cast<char*>(ptr) << '\n'; // cast to char pointer (no dereference)
+            // std::cout knows to treat char* as a C-style string
+            // if we were to dereference the result, then we'd just print the single char that ptr is pointing to
+            break;
+    }
+}
+ 
+int main()
+{
+    int nValue{ 5 };
+    float fValue{ 7.5f };
+    char szValue[]{ "Mollie" };
+ 
+    printValue(&nValue, Type::INT);
+    printValue(&fValue, Type::FLOAT);
+    printValue(szValue, Type::CSTRING);
+ 
+    return 0;
+}
+*/
