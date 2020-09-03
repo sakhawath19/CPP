681ad0f293782eb5762dbc95c5590e7e456d0acc
sakhawath <sakhawath19@gmail.com>
2020-09-03 10:58:50 +0300

    updated

---
+++ L19_template.cpp
@@ -0,0 +1,53 @@
+#include <iostream> 
+using namespace std; 
+
+template <typename T> 
+class Array { 
+private: 
+	T *ptr; 
+	int size; 
+public: 
+	Array(T arr[], int s); 
+	void print(); 
+}; 
+
+template <typename T> 
+Array<T>::Array(T arr[], int s) { 
+	ptr = new T[s]; 
+	size = s; 
+	for(int i = 0; i < size; i++) 
+		ptr[i] = arr[i]; 
+} 
+
+template <typename T> 
+void Array<T>::print() { 
+	for (int i = 0; i < size; i++) 
+		cout<<" "<<*(ptr + i); 
+	cout<<endl; 
+} 
+
+int main() { 
+	int arr[5] = {1, 2, 3, 4, 5}; 
+	Array<int> a(arr, 5); 
+	a.print(); // 1 2 3 4 5
+	return 0; 
+} 
+
+/*
+#include<iostream> 
+using namespace std; 
+
+template<class T, class U> 
+class A { 
+	T x; 
+	U y; 
+public: 
+	A() { cout<<"Constructor Called"<<endl; } 
+}; 
+
+int main() { 
+A<char, char> a;  // Constructor called
+A<int, double> b; // Constructor called
+return 0; 
+} 
+*/
